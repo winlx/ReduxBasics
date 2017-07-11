f8dd4b77630cc57db05da1636c54a5c95acdd98c
Aleksei Iakovets <aleksei@winlx.com>
2017-07-11 23:51:39 +0300

    First commit in the branch adding-filter

---
+++ src/components/Filter.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import FilterLink from './FilterLink';
+
+function Filter(props) {
+  return (
+    <div className="todo-filter">
+      <FilterLink
+        icon="list"
+        active={props.activeFilter === 'ALL'}
+        onClick={() => props.onSetFilter('ALL')}
+      />
+
+      <FilterLink
+        icon="check_box"
+        active={props.activeFilter === 'COMPLETED'}
+        onClick={() => props.onSetFilter('COMPLETED')}
+      />
+
+      <FilterLink
+        icon="check_box_outline_blank"
+        active={props.activeFilter === 'UNCOMPLETED'}
+        onClick={() => props.onSetFilter('UNCOMPLETED')}
+      />
+    </div>
+  );
+}
+
+Filter.propTypes = {
+  activeFilter: PropTypes.string.isRequired,
+  onSetFilter: PropTypes.func.isRequired,
+};
+
+export default Filter;
---
+++ src/components/FilterLink.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function FilterLink(props) {
+  return (
+    <a className={props.active ? 'is-active' : ''} onClick={props.onClick}>
+      <i className="material-icons">{props.icon}</i>
+    </a>
+  );
+}
+
+FilterLink.propTypes = {
+  icon: PropTypes.string.isRequired,
+  active: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default FilterLink;
