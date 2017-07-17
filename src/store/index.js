df109a60cab88a91365eec4a94933807557841cd
Aleksei Iakovets <aleksei@winlx.com>
2017-07-18 02:32:37 +0300

    Async has been learned

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import thunkMiddleware from 'redux-thunk';
 
 import reducer from '../reducers/reducer';
 
-function addPromiseThunkSupport(store) {
-  const dispatch = store.dispatch;
-
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(dispatch);
-    } else if (typeof action === 'function') {
-      return action(dispatch);
-    }
-
-    return dispatch(action);
-  };
-}
-
-const store = createStore(reducer);
-
-store.dispatch = addPromiseThunkSupport(store);
+const store = createStore(reducer, applyMiddleware(promiseMiddleware, thunkMiddleware));
 
 export default store;
